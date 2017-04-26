bce1da0f3fdd925ce3cc105970b6c06474734ec1
Joachim Lovf <joachim.lovf@creuna.no>
2017-04-26 09:49:31 +0200

    Add project files.

---
+++ SitemapCrawler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SitemapCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
